37879a2fb77f05209db4ca4d0a078219631bb454
Binh05 <binhdt0452@gmail.com>
2026-03-15 10:00:00 +0700

    refactor: refactor app folder structure

--- src/app/(with-sidebar)/layout.tsx
+++ src/app/(with-sidebar)/layout.tsx
@@ -6,14 +6,18 @@ export const iframeHeight = "800px";
 
 export const description = "A sidebar with a header and a search form.";
 
-export default function Page() {
+export default function SidebarLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="[--header-height:calc(--spacing(14))]">
       <SidebarProvider className="flex flex-col">
         <SiteHeader />
         <div className="flex flex-1">
           <AppSidebar />
-          <SidebarInset>{/* Content */}</SidebarInset>
+          <SidebarInset>{children}</SidebarInset>
         </div>
       </SidebarProvider>
     </div>
