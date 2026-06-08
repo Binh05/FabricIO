c6b080a9b0f110816e5a3ef57e2304250f18a87c
Binh05 <binhdt0452@gmail.com>
2026-06-08 17:05:25 +0700

    feat: thêm toast log lỗi và thành công

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,36 +10,44 @@ import { GameReview } from "./pages/GameReview";
 import { Profile } from "./pages/Profile";
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
     <AppProvider>
+      <Toaster />
       <div className="bg-background text-foreground min-h-screen">
-        <Router>
-          <Routes>
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route
-              path="*"
-              element={
-                <MainLayout>
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/games" element={<Games />} />
-                    <Route path="/game-detail/:id" element={<GameDetail />} />
-                    <Route path="/play/:id" element={<Play />} />
-                    <Route path="/submit-game" element={<SubmitGame />} />
-                    <Route path="/game-review" element={<GameReview />} />
-                    <Route path="/profile" element={<Profile />} />
-                  </Routes>
-                </MainLayout>
-              }
-            />
-          </Routes>
-        </Router>
+        <AppRouter />
       </div>
     </AppProvider>
   );
 }
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="*"
+          element={
+            <MainLayout>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/games" element={<Games />} />
+                <Route path="/game-detail/:id" element={<GameDetail />} />
+                <Route path="/play/:id" element={<Play />} />
+                <Route path="/submit-game" element={<SubmitGame />} />
+                <Route path="/game-review" element={<GameReview />} />
+                <Route path="/profile" element={<Profile />} />
+              </Routes>
+            </MainLayout>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -3,54 +3,83 @@ import { authService } from "@/services/authService";
 import { userService } from "@/services/userService";
 import type { RegisterForm } from "@/types/Auth";
 import { useCallback, useContext } from "react";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 export const useAuth = () => {
-    const navigate =  useNavigate();
-    const context = useContext(AuthContext)
+  const navigate = useNavigate();
+  const context = useContext(AuthContext);
 
-    if (!context) {
-        throw new Error("useAuth phải được sử dụng bên trong AuthProvider");
+  if (!context) {
+    throw new Error("useAuth phải được sử dụng bên trong AuthProvider");
+  }
+
+  const signUp = useCallback(
+    async (formData: RegisterForm) => {
+      try {
+        await authService.resgister(formData);
+
+        navigate("/signin");
+        toast.success("Đăng ký thành công");
+      } catch (error) {
+        console.error("Lỗi khi signup", error);
+        toast.error(
+          error?.response?.data?.message ?? "Đã xảy ra lỗi. Hãy thử lại!",
+        );
+      }
+    },
+    [navigate],
+  );
+
+  const login = useCallback(
+    async (username: string, password: string) => {
+      try {
+        localStorage.clear();
+        context.clearAuthState();
+
+        const { data } = await authService.login({ username, password });
+
+        context.setToken(data.accessToken);
+        localStorage.setItem("access_token", data.accessToken);
+
+        const { data: user } = await userService.fetchMe();
+        context.setUser(user);
+
+        navigate("/");
+        toast.success("Đăng nhập thành công");
+      } catch (error) {
+        console.error("Lỗi khi signin", error);
+        toast.error(
+          error?.response?.data?.message ?? "Đã xảy ra lỗi. Hãy thử lại!",
+        );
+      }
+    },
+    [navigate],
+  );
+
+  const signOut = useCallback(async () => {
+    try {
+      await authService.signOut();
+
+      localStorage.clear();
+      context.clearAuthState();
+
+      navigate("/");
+      toast.success("Đã đăng xuất");
+    } catch (error) {
+      console.error("Lỗi khi sign out", error);
+      toast.error(
+        error?.response?.data?.message ?? "Đã xảy ra lỗi. Hãy thử lại!",
+      );
     }
+  }, [navigate]);
 
-    const signUp = useCallback(async (formData: RegisterForm) => {
-        try {
-            await authService.resgister(formData)
-            navigate("/signin")
-        } catch (error) {
-            
-        }
-    }, [navigate])
-
-    const login = useCallback(async (username: string, password: string) => {
-        try {
-            localStorage.clear()
-            const { data } = await authService.login({username, password})
-
-            context.setToken(data.accessToken)
-            localStorage.setItem("access_token", data.accessToken)
-
-            const { data: user } = await userService.fetchMe()
-            context.setUser(user)
-
-            navigate("/")
-        } 
-        catch (error) 
-        {
-        }
-    }, [navigate])
-
-    const signOut = useCallback(async () => {
-        try {
-            await authService.signOut()
-
-            context.setToken(null)
-            localStorage.clear()
-            navigate("/")
-        } catch (error) {
-            
-        }
-    }, [navigate])
-
-    return { user: context.user, token: context.token, loading: context.loading, login, signOut, signUp }
-}
+  return {
+    user: context.user,
+    token: context.token,
+    loading: context.loading,
+    login,
+    signOut,
+    signUp,
+  };
+};
